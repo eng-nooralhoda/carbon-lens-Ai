215aafe86a8677e536b35b10de25fc53ebeb5ccf
eng-nooralhoda <ghaldon46@gmail.com>
2025-08-15 00:09:24 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,4 @@
 import streamlit as st
 
-st.title('🎈 App Name')
-
-st.write('Hello world!')
+st.title('🤖 cabon lens AI')
+st.info('carbon lens AI is a cloud- powered machine learning tool that predicts CO2 emissions based on energy consumption data')
