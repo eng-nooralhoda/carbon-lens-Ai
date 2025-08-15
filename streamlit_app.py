42e8cf38924c7983f63248dcc9f2ad7d69ec9243
eng-nooralhoda <ghaldon46@gmail.com>
2025-08-15 03:45:57 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,7 +14,7 @@ with st.expander('Data'):
   X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42) 
   model = RandomForestRegressor(n_estimators=100, random_state=42) 
   model.fit
- (X_train, y_train) 
+  (X_train, y_train) 
  y_pred = model.predict(X_test) 
 r2 = r2_score(y_test, y_pred) 
 rmse = root_mean_squared_error(y_test, y_pred) 
