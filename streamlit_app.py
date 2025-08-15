8930570fcdf910d4316d94781707675178e851bc
eng-nooralhoda <ghaldon46@gmail.com>
2025-08-15 03:20:58 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,6 @@
 import streamlit as st
+import pandas as pd
 st.title('🌍carbon lens AI')
 st. info('carbon lens AI is a cloud-powered machine learning tool that predicts CO2 emissions based on energy consumption data')
-
+df =pd. read_csv('https://raw.githubusercontent.com/eng-nooralhoda/carbon-lens-Ai/refs/heads/master/CO2_cleaned_1950_onwards.csv')
+df
