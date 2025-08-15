d8ea804875bd3defe35a1c141bb3c0dee9792aeb
eng-nooralhoda <ghaldon46@gmail.com>
2025-08-15 04:07:11 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,8 @@ with st.expander('Data'):
   y = df['Total'] 
   from sklearn.model_selection import train_test_split
   X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-  model = RandomForestRegressor(n_estimators=100, random_state=42) 
+  from sklearn.ensemble import RandomForestRegressor 
+  model = RandomForestRegressor(n_estimators=100, random_state=42)
   model.fit
   (X_train, y_train) 
   y_pred = model.predict(X_test) 
