82bba966410d70686549286598a72088165436ae
eng-nooralhoda <ghaldon46@gmail.com>
2025-08-15 04:04:35 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,8 @@ with st.expander('Data'):
   df['Country_Code']=  df['Country_Code'].astype('category').cat.codes
   X = df[['Year', 'Country_Code', 'Coal', 'Oil', 'Gas', 'Cement', 'Flaring', 'Other']] 
   y = df['Total'] 
-  X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42) 
+  from sklearn.model_selection import train_test_split
+  X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
   model = RandomForestRegressor(n_estimators=100, random_state=42) 
   model.fit
   (X_train, y_train) 
