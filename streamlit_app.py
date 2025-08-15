75bf326d5b5c5184620f584bce9d4a4d10f5a003
eng-nooralhoda <ghaldon46@gmail.com>
2025-08-15 03:42:49 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,16 +6,16 @@ with st.expander('Data'):
   st.write('**data set**')
   df =pd. read_csv('https://raw.githubusercontent.com/eng-nooralhoda/carbon-lens-Ai/refs/heads/master/CO2_cleaned_1950_onwards.csv')
   df
- df = pd.read_csv('https://raw.githubusercontent.com/eng-nooralhoda/carbon-lens-Ai/refs/heads/master/CO2_cleaned_1950_onwards.csv') 
-df['Country_Code'] = df['Country'].astype('category').
- cat.codes
- X = df[['Year', 'Country_Code', 'Coal', 'Oil', 'Gas', 'Cement', 'Flaring', 'Other']] 
-y = df['Total'] 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42) 
-model = RandomForestRegressor(n_estimators=100, random_state=42) 
-model.fit
+  df = pd.read_csv('https://raw.githubusercontent.com/eng-nooralhoda/carbon-lens-Ai/refs/heads/master/CO2_cleaned_1950_onwards.csv') 
+  df['Country_Code'] = df['Country'].astype('category').
+  cat.codes
+  X = df[['Year', 'Country_Code', 'Coal', 'Oil', 'Gas', 'Cement', 'Flaring', 'Other']] 
+  y = df['Total'] 
+  X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42) 
+  model = RandomForestRegressor(n_estimators=100, random_state=42) 
+  model.fit
  (X_train, y_train) 
-y_pred = model.predict(X_test) 
+ y_pred = model.predict(X_test) 
 r2 = r2_score(y_test, y_pred) 
 rmse = root_mean_squared_error(y_test, y_pred) 
 feature_importance = pd.Series(model.feature_importances_, 
